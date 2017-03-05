068c31584a0cee50fdd1a064f7a28b66c0286c26
越空行者 <hongfeiyu@mail.cnu.edu.pl>
2017-03-05 09:21:09 +0800

    init

---
+++ NeteaseCloudMusic-Download.py
@@ -0,0 +1,14 @@
+# -*- coding:UTF-8 -*-
+# Author：hongfeiyu
+import os
+path=raw_input("请输入缓存文件目录并彻底退出播放器： ")#.strip()
+path=path+"/Cache"
+#print path
+files = os.listdir(path)
+for f in files:
+    file_name, file_extname = f.split('.')
+    print file_name, file_extname
+    if file_extname == 'uc':
+    	os.chdir(path)#切记要到目录下来修改
+        os.rename(f, '%s.mp3' %file_name)
+print u'文件下载完成！请打开播放器手动上传到云端！'
